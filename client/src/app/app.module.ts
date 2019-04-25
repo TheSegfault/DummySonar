de66c7856cd06941ec9257bb9bafa02a7827ab2e
Alexy Hody <Alexy.hody@hotmail.fr>
2019-04-25 22:00:24 +0200

    ng add @ngrx/router-store

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { CoreModule } from './core/core.module';
   imports: [
     AppRoutingModule,
     CoreModule,
+    StoreRouterConnectingModule.forRoot(),
   ],
   bootstrap: [ AppComponent ]
 })
