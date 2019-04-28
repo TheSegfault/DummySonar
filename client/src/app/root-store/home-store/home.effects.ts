fa2439e78cc68551c96ec94ed3aa5cb29be07bc1
Alexy Hody <Alexy.hody@hotmail.fr>
2019-04-28 19:41:14 +0200

    Adding fake service

--- client/src/app/root-store/home-store/home.effects.ts
+++ client/src/app/root-store/home-store/home.effects.ts
@@ -2,16 +2,25 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import * as featureActions from './home.actions';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import * as homeActions from './home.actions';
+import { AuthService } from '../../services/auth.service';
 
 @Injectable()
 export class HomeStoreEffects {
   @Effect()
-  loginRequestEffect$: Observable<Action> = this.actions$.pipe(
-    ofType<featureActions.LoginRequestAction>(featureActions.ActionTypes.LOGIN_REQUEST),
-    switchMap(action => of(new featureActions.LoginSuccessAction({ user: { user: 'coucou' } })))
-  );
+  loginRequestEffect$: Observable<Action> = this.actions$
+    .pipe(
+      ofType<homeActions.LoginRequestAction>(homeActions.ActionTypes.LOGIN_REQUEST),
+      switchMap(action =>
+        AuthService.login(action.payload.userName, action.payload.password)
+          .pipe(
+            map(user => new homeActions.LoginSuccessAction({ user })),
+            catchError(error => of(new homeActions.LoginFailureAction({ error }))
+            )
+          )
+      )
+    );
 
   constructor(private actions$: Actions) {
   }
---
+++ client/src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+// TODO: Move this to a model file
+export interface User {
+  user: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+  public static login(userName: string, password: string): Observable<User> {
+    return of({ user: userName } as User);
+  }
+}
