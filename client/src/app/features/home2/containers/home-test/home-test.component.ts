7a34995f740c0488a744cd64217af93e51a9b4ff
Alexy Hody <Alexy.hody@hotmail.fr>
2019-04-28 21:42:55 +0200

    Cleaning home-test

--- client/src/app/features/home2/containers/home-test/home-test.component.ts
+++ client/src/app/features/home2/containers/home-test/home-test.component.ts
@@ -1,10 +1,4 @@
 import { ChangeDetectionStrategy, Component, OnInit, } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-
-import { HomeStoreActions, HomeStoreSelectors } from '@features/home/store';
-import { User } from '@models/User';
-import { RootState } from '@rootStore/state';
 
 @Component({
   selector: 'app-home2',
@@ -13,20 +7,9 @@ import { RootState } from '@rootStore/state';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeTestComponent implements OnInit {
-  public $error: Observable<string>;
-  public $isLoading: Observable<boolean>;
-  public $user: Observable<User>;
-
-  constructor(private $store: Store<RootState>) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.$error = this.$store.select(HomeStoreSelectors.selectHomeError);
-    this.$isLoading = this.$store.select(HomeStoreSelectors.selectHomeIsLoading);
-    this.$user = this.$store.select(HomeStoreSelectors.selectHomeUser);
-
-    // TODO: Put this out of the ngOnInit
-    // TODO: Dont forget to use a guard in order to check if connected for each route
-    this.$store.dispatch(new HomeStoreActions.LoginRequestAction({ userName: 'coucou', password: 'coucou' }));
   }
 }
