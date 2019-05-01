e0dcc30c6093c8cbf627de285ffab9d67948fe51
Alexy Hody <Alexy.hody@hotmail.fr>
2019-05-01 22:43:49 +0200

    Adding FxLayout

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './containers/home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '@shared/shared.module';
+import { FlexModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     HomeRoutingModule,
     SharedModule,
+    FlexModule,
   ],
   declarations: [ HomeComponent ]
 })
