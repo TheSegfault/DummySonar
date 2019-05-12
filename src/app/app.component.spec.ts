0206a47d307e165613b83a78ad706a9fa5c7d76b
Alexy Hody <Alexy.hody@hotmail.fr>
2019-05-12 11:38:38 +0200

    Change project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DummySonar'`, () => {
+  it(`should have as title 'NgrxCore'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('DummySonar');
+    expect(app.title).toEqual('NgrxCore');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to DummySonar!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to NgrxCore!');
   });
 });
